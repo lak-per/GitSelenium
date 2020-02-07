2d172da4928133440d04c272e001b8f3caea3dca
lakshmana <laxman.forpersonal@gmail.com>
2020-02-07 14:04:56 +0530

    X person is commited

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -10,6 +10,9 @@ public class RestAPITest {
 		System.out.println("PostJIRA");
 		System.out.println("PostJIRA2");
 		System.out.println("PostJIRA3");
+		System.out.println("PostJIRA4");
+		System.out.println("PostJIRA5");
+		System.out.println("PostJIRA6");
 	}
 	@Test
 	public void deleteTwitter()
