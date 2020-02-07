7c8a18896a90f61b99d356179a342a65b2da9a97
lakshmana <laxman.forpersonal@gmail.com>
2020-02-07 12:30:58 +0530

    my first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("PostJIRA");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("ElementsUI");
+	}
+
+}
